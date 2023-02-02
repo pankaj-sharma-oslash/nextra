a5129abacde3aa79969111385e8e906f94373837
pankaj-sharma-oslash <pankaj.sharma@oslash.com>
2023-02-02 13:03:08 +0530

    first commit

---
+++ theme.config.jsx
@@ -0,0 +1,15 @@
+const theme = {
+  logo: <img src="https://global-uploads.webflow.com/60c0cec90f5782a19af55867/60c0cec90f57822da6f55955_oslsh.svg" style={{height:40}} />,
+  project: {
+    link: 'https://github.com/realtimeskills/oslash',
+
+  },
+  toc: {
+    extraContent: <img src="#" alt="" />,
+    float: true,
+  },
+}
+
+export default theme
+
+
